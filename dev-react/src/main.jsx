542e4744791e0f3ced1285f8e386ba3bdc1ace23
Jade Jolivet <jade.jolivet@gmail.com>
2024-07-03 12:18:35 +0200

    app architecture

--- dev-react/src/main.jsx
+++ dev-react/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <App />,
+
+    children: [
+      { path: "/", element: <HomePage /> },
+      {
+        path: "/program",
+        element: <programPage />,
+      },
+      {
+        path: "/event",
+        element: <EventPage />,
+      },
+      {
+        path: "/profil",
+        element: <ProfilPage />,
+      },
+      {
+        path: "/connexion",
+        element: <LoginPage />,
+      },
+      {
+        path: "/inscription",
+        element: <RegistrationPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
