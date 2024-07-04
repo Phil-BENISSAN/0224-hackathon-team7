b191d553988565a26d6c3d6eeb33576bae8189a6
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2024-07-04 12:11:45 +0200

    update

--- dev-react/src/App.jsx
+++ dev-react/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
+    <>
   <Outlet />
+  <Footer />
+  </>
   )
 }
 
--- dev-react/src/components/Footer/Footer.jsx
+++ dev-react/src/components/Footer/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import "./footer.css"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <section className="footer-section">
+    <img src="../../src/assets/images/logo-footer.png"/>
+    <section className="footer-subsection">
+    <article className="footer-article1">
+    <p>Contactez-nous !</p>
+    <p>01 23 45 67 89</p>
+    <p>inclus-event@gmail.com</p>
+    <p>Mentions légales</p>
+    <p>Politique de confitiendalité</p>
+    </article>
+    <article className="footer-article2">
+<p>Suivez-nous !</p>
+<figure>
+<img src="../../src/assets/images/TwitterX.png"/>
+<img src="../../src/assets/images/Facebook.png"/>
+<img src="../../src/assets/images/Instagram.png"/>
+<img src="../../src/assets/images/Linkedin.png"/>
+</figure>
+    </article>
+    </section>
+    </section>
   )
 }
 
