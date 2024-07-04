8320c6844ce36fae9ca6fc9c2dd12b208afc0098
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2024-07-04 11:11:00 +0200

    add images

--- dev-react/src/pages/ProgramPage/ProgramPage.jsx
+++ dev-react/src/pages/ProgramPage/ProgramPage.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import "./programPage.css"
+import React from "react";
+import eventData from "../../assets/json/confData.json";
+import "./programPage.css";
 
 function ProgramPage() {
   return (
     <>
-    <h2 className="page-main-title">PROGRAMME</h2>
-    <button
-    type = "button">
-      Filtrer
-    </button>
-    {/* filtres */}
-    {/* sliders par thème : avec image et titre de l'événement dessus 
+      <h2 className="page-main-title">PROGRAMME</h2>
+      <button type="button">Filtrer</button>
+      {/* filtres */}
+      {/* sliders par thème : avec image et titre de l'événement dessus 
 
-    - mettre un link pour renvoi vers la page de l'image qu'on veut
+    - mettre un link pour renvoi vers la page de l'image qu'on veut    */}
 
-    events.map((event) => {
-   
-      <h3>event.theme</h3>
-         <figure>
-      <img src=event.image alt=""/>
-      <figcaption>event.title</figcaption>
-      </figure>
-    } )
-    
-    */}
+      {eventData.map((event) => (
+        <>
+          <h3 key={event.titre}>{event.theme ? event.theme : "test"}</h3>
+          {/* penser à changer la key en id */}
+            <figure>
+              <img src={event.image} alt="" />
+              <figcaption>{event.titre}</figcaption>
+            </figure>
+        </>
+      ))}
     </>
-
-  )
+  );
 }
 
-export default ProgramPage
+export default ProgramPage;
